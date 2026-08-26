eb50bfe692a4343a09e733f1a146d0ac3a5e6750
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-08-27 01:29:44 +1000

    Document VM diagnostics headroom

--- frontend_tests/frontend_vm32_test.go
+++ frontend_tests/frontend_vm32_test.go
@@ -116,6 +116,7 @@ func TestVM32FrontendPerformanceGate(t *testing.T) {
 		output[1] != 'E' || output[2] != 'L' || output[3] != 'F' {
 		t.Fatalf("VM frontend Linux output prefix = % x", output[:minBundleLength(len(output), 4)])
 	}
+	// Keep enough execution headroom for richer self-hosted diagnostics.
 	if len(image) > 6*1024*1024 ||
 		len(output) > 4*1024*1024 ||
 		compileResult.Steps > 12*1000*1000*1000 ||
