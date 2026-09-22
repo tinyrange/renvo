ae2eecb4035a294ac4076b5cda25c355b13ed549
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-09-22 16:37:14 +1000

    Reuse ARM frontend scratch memory for embedded backend emission

--- internal/driver/arena_other.go
+++ internal/driver/arena_other.go
@@ -1,4 +1,4 @@
-//go:build renvo && !(linux && amd64) && !(darwin && arm64) && !(vm && vm32) && !(wasi && wasm32)
+//go:build renvo && !(linux && (amd64 || arm)) && !(darwin && arm64) && !(vm && vm32) && !(wasi && wasm32)
 
 package driver
 
